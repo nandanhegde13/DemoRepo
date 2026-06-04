f744eada489f394c62aa9b03c13708c3dfe7597c
Nandan <hegdenandan013@gmail.com>
2026-06-04 18:11:54 +0530

    branch commit

--- src/TestGithubPackage/TestClass.java
+++ src/TestGithubPackage/TestClass.java
@@ -4,6 +4,10 @@ public class TestClass {
 
 	public static void main(String[] args) {
       System.out.println("Testing github package upload");
+      System.out.println("Testing github package upload");
+      System.out.println("Testing github package upload");
+      System.out.println("Testing github package upload");
+      
 	}
 
 }
